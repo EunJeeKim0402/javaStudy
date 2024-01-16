7f1866084f5b766799fef61c377dcafcf972e62c
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2024-01-16 21:20:07 +0900

    java스터디
    java스터디

---
+++ src/org/opentutorials/javatutorials/condition/Condition2Demo.java
@@ -0,0 +1,17 @@
+package org.opentutorials.javatutorials.condition;
+
+public class Condition2Demo {
+
+	public static void main(String[] args) {
+
+		if(true) { // false면 5만 출력됨
+			System.out.println(1);
+			System.out.println(2);
+			System.out.println(3);
+			System.out.println(4);
+		}
+		System.out.println(5);
+
+	}
+
+}
---
+++ src/org/opentutorials/javatutorials/condition/Condition3Demo.java
@@ -0,0 +1,16 @@
+package org.opentutorials.javatutorials.condition;
+
+public class Condition3Demo {
+
+	public static void main(String[] args) {
+
+		// 만약 true가 여러개면 첫번째 true만 실행됨
+		if(true) { 
+			System.out.println(1);
+		} else {
+			System.out.println(2);
+		}
+
+	}
+
+}
