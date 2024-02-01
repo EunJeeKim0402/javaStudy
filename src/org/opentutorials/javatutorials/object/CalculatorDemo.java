17f16f41902718cfe0f640501056f3065aad21e1
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2024-02-01 22:26:55 +0900

    객체지향에대한 설명
    객체지향에대한 설명

---
+++ src/org/opentutorials/javatutorials/object/CalculatorDemo.java
@@ -0,0 +1,19 @@
+package org.opentutorials.javatutorials.object;
+
+import org.opentutorials.javatutorials.io.만드는;
+import org.opentutorials.javatutorials.io.상태와;
+import org.opentutorials.javatutorials.io.서로;
+import org.opentutorials.javatutorials.io.이루어진;
+
+public class CalculatorDemo {
+
+	public static void main(String[] args) {
+
+		//객체지향프로그래밍(OOP : Object Oriented Programming)
+		//서로 연관되어있는 상태와 행위로 이루어진 객체를 만드는 프로그램
+		System.out.println(10 + 20);
+		System.out.println(20 + 40);
+
+	}
+
+}
---
+++ src/org/opentutorials/javatutorials/object/CalculatorDemo2.java
@@ -0,0 +1,21 @@
+package org.opentutorials.javatutorials.object;
+
+import org.opentutorials.javatutorials.io.만드는;
+import org.opentutorials.javatutorials.io.상태와;
+import org.opentutorials.javatutorials.io.서로;
+import org.opentutorials.javatutorials.io.이루어진;
+
+public class CalculatorDemo2 {
+	
+	public static void sum(int left, int right) {
+		System.out.println(left + right);
+	}
+
+	public static void main(String[] args) {
+
+		sum(10, 20);
+		sum(20, 40);
+
+	}
+
+}
