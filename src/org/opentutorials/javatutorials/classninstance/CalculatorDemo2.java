792749e49523832aa038907d5a1e7c8698c88815
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2024-02-04 16:24:06 +0900

    클래스, 메소드, 인스턴스
    클래스, 메소드, 인스턴스

--- src/org/opentutorials/javatutorials/classninstance/CalculatorDemo2.java
+++ src/org/opentutorials/javatutorials/classninstance/CalculatorDemo2.java
@@ -1,10 +1,11 @@
-package org.opentutorials.javatutorials.object;
+package org.opentutorials.javatutorials.classninstance;
 
 class Calculator {
+	static double PI = 3.14; // static변수를 붙임으로써 해당 클래스 소속이라는것
 	int left, right;
 	
 	public void setOprands(int left, int right) {
-		this.left = left; // this란 해당 변수를 가리키는것
+		this.left = left;
 		this.right = right;
 	}
 	
@@ -17,19 +18,17 @@ class Calculator {
 	}
 }
 
-public class CalculatorDemo4 {
+public class CalculatorDemo2 {
 	
 	public static void main(String[] args) {
 
 		Calculator c1 = new Calculator();
-		c1.setOprands(10, 20);
-		c1.sum();
-		c1.avg();
+		System.out.println(c1.PI);
 		
 		Calculator c2 = new Calculator();
-		c2.setOprands(20, 40);
-		c2.sum();
-		c2.avg();
+		System.out.println(c2.PI);
+		
+		System.out.println(Calculator.PI);
 		
 	}
 
