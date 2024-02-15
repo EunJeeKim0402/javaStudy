005363475de6338d41eb0d98772937969c5aaecd
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2024-02-15 21:07:16 +0900

    접근제어자, 패키지
    접근제어자, 패키지

---
+++ src/org/opentutorials/javatutorials/packages/example3/D.java
@@ -0,0 +1,13 @@
+package org.opentutorials.javatutorials.packages.example3;
+
+import org.opentutorials.javatutorials.packages.example1.*;
+import org.opentutorials.javatutorials.packages.example2.*;
+
+public class D {
+
+	public static void main(String[] args) {
+		//B b = new B(); ambiguous 에러
+		org.opentutorials.javatutorials.packages.example1.B b = new org.opentutorials.javatutorials.packages.example1.B();
+	}
+
+}
