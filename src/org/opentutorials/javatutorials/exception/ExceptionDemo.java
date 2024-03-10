4c162172ded7e6514fb600c8d9e1cae39c06149f
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2024-03-10 23:37:03 +0900

    예외처리, finally, 빠른 예외처리
    예외처리, finally, 빠른 예외처리

--- src/org/opentutorials/javatutorials/exception/ExceptionDemo.java
+++ src/org/opentutorials/javatutorials/exception/ExceptionDemo.java
@@ -8,7 +8,15 @@ class A {
 		arr[2] = 20;
 	}
 	public void z(int first, int second) {
-		System.out.println(arr[first] / arr[second]);
+		try {
+			System.out.println(arr[first] / arr[second]);
+		} catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("ArrayIndexOutOfBoundsException");
+		} catch(Exception e) {
+			System.out.println("Exception");
+		} finally {
+			System.out.println("finally");
+		}
 	}
 }
 
@@ -16,7 +24,11 @@ public class ExceptionDemo {
 
 	public static void main(String[] args) {;
 		A a = new A();
-		a.z(10, 1);
+		a.z(10, 0);
+		a.z(1, 0);
+		a.z(2, 1);
 	}
 
 }
+
+
