774d5244c2a6a11798caf00052130b3e7a4a00f0
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2024-03-23 23:07:41 +0900

    equals
    equals

---
+++ src/org/opentutorials/javatutorials/progenitor/ObjectDemo.java
@@ -0,0 +1,26 @@
+package org.opentutorials.javatutorials.progenitor;
+
+class Student {
+	String name;
+	Student(String name) {
+		this.name = name;
+	}
+	public boolean equals(Object obj) {
+		//Student s = obj; 에러남
+		Student s = (Student)obj;
+		return this.name == s.name;
+	}
+}
+
+public class ObjectDemo {
+
+	public static void main(String[] args) {
+		
+		Student s1 = new Student("egoing");
+		Student s2 = new Student("egoing");
+		System.out.println(s1 == s2); // false
+		System.out.println(s1.equals(s2)); // false
+
+	}
+
+}
