b723681709ad171f859e6a22d3163ceaa32cc3c9
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2024-07-10 07:40:00 +0900

    수정

--- src/org/java/Hello.java
+++ src/org/java/Hello.java
@@ -1,6 +1,6 @@
 package org.java;
 class Hello {
-    public static void main(String[] args) {
+    public static void main(String[] args) { // main메소드의 선언부
         System.out.println("Hello world");
     }
 }
