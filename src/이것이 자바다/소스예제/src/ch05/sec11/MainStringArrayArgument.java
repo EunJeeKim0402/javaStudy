67bde51386443beaffc5243136c8fa1e399412c8
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2024-07-29 07:32:31 +0900

    enum백업
    enum백업

--- src/이것이 자바다/소스예제/src/ch05/sec11/MainStringArrayArgument.java
+++ src/이것이 자바다/소스예제/src/ch05/sec11/MainStringArrayArgument.java
@@ -17,3 +17,5 @@ public class MainStringArrayArgument {
         System.out.println(num1 + " + " + num2 + " = " + result);
     }
 }
+{
+}
---
+++ src/이것이 자바다/소스예제/src/ch05/sec12/Week1.java
@@ -0,0 +1,30 @@
+package ch05.sec12;
+
+public enum Week {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
+
+public enum Week1 {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
+public enum Week2 {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
